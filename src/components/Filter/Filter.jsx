9651eded0e9379a1d0b7a434d7a166eb1e5a7489
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-01-18 17:41:03 +0200

    1

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -11,6 +11,7 @@ const Filter = ({ value, onChange }) => (
         value={value}
         onChange={onChange}
         className={s.filterInput}
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$ \+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
       />
     </label>
   </div>
